166c7ed99c1013e23bd523d7d3890f6f98fb2f0f
joacasallas2 <proyecta.ds@gmail.com>
2024-01-27 02:12:24 -0500

    fix only spaces input

--- 1-read_line.c
+++ 1-read_line.c
@@ -9,10 +9,28 @@ char *read_line(void)
 	ssize_t readed;
 	char *line_ptr = NULL;
 	size_t n = 0;
+	int i = 0, flag = 1;
 
 	/*Reads from the standard input using getline.*/
 	readed = getline(&line_ptr, &n, stdin);
-
+	if (line_ptr[0] == 32)
+	{
+		flag = 0;
+		while (line_ptr[i] == 32)
+		{
+			i++;
+			if (line_ptr[i] != 32 && line_ptr[i] != 10)
+			{
+				flag = 1;
+				break;
+			}
+		}
+	}
+	if (flag == 0)
+	{
+		free(line_ptr);
+		return (NULL);
+	}
 	/*Handles end-of-file conditions or errors*/
 	if (readed == -1)
 	{
--- shell.c
+++ shell.c
@@ -25,12 +25,15 @@ int main(int ac, char **av, char **env)
 		}
 		/*Reads input and store in line_ptr*/
 		line_ptr = read_line();
-		/*Tokenizes the input and store in commands*/
-		commands = tokenizer(line_ptr, " \n");
-		free(line_ptr);
-		/*Executes the commands*/
-		_exec(commands, env);
-		free_array(commands);
+		if (line_ptr != NULL)
+		{
+			/*Tokenizes the input and store in commands*/
+			commands = tokenizer(line_ptr, " \n");
+			free(line_ptr);
+			/*Executes the commands*/
+			_exec(commands, env);
+			free_array(commands);
+		}
 	}
 	return (0);
 }
